ffd9d0433236243a29cdd51e01e7bcdece9dedc9
xtang <xtang@avos.com>
2012-11-15 02:01:58 +0800

    update emacs config

---
+++ emacs/dotemacs.d/elpa/google-c-style-20091009.1651/google-c-style-autoloads.el
@@ -0,0 +1,18 @@
+;;; google-c-style-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("google-c-style-pkg.el" "google-c-style.el")
+;;;;;;  (20643 55752 889103))
+
+;;;***
+
+(provide 'google-c-style-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; google-c-style-autoloads.el ends here
