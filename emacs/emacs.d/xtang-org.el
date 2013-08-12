8eeb6f801e81ed51f9f483d25f280287c86069fe
Xiaomin Tang <xtang@avos.com>
2013-08-12 10:41:03 +0800

    unset M-RET in org mode

--- emacs/emacs.d/xtang-org.el
+++ emacs/emacs.d/xtang-org.el
@@ -1,4 +1,5 @@
 (add-hook 'org-mode-hook '(lambda () (progn
-                                 (local-unset-key (kbd "M-<return>")))))
+                                 (local-unset-key (kbd "M-<return>"))
+                                 (local-unset-key (kbd "M-RET")))))
 (setq org-log-done t)
 (provide 'xtang-org)
