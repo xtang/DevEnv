33162de0f6222586f5eb30063f7b793640127526
Xiaomin Tang <xtang@avos.com>
2013-08-19 17:34:22 +0800

    add c-common-hook

--- emacs/emacs.d/xtang-cpp.el
+++ emacs/emacs.d/xtang-cpp.el
@@ -1,5 +1,7 @@
-(add-hook 'c++-mode-hook (lambda ()
-                           (progn
-                             (local-set-key (kbd "M-,") 'pop-tag-mark))))
+(dolist (hook '(c++-mode-hook c-mode-common-hook))
+  (add-hook hook
+            (lambda ()
+              (progn
+                (local-set-key (kbd "M-,") 'pop-tag-mark)))))
 
 (provide 'xtang-cpp)
