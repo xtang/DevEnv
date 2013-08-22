2da2c1e2994aa85459b9632b4883a32afc872365
Xiaomin Tang <xtang@avos.com>
2013-08-22 19:52:54 +0800

    add c/cpp indent rule

--- emacs/emacs.d/xtang-cpp.el
+++ emacs/emacs.d/xtang-cpp.el
@@ -2,6 +2,7 @@
   (add-hook hook
             (lambda ()
               (progn
-                (local-set-key (kbd "M-,") 'pop-tag-mark)))))
+                (local-set-key (kbd "M-,") 'pop-tag-mark)
+                (setq c-basic-offset 4)))))
 
 (provide 'xtang-cpp)
