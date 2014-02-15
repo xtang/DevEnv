6a9370e7398003ea5b2ae24287b1c836969c15a1
xtang <xiaom.tang@gmail.com>
2014-02-16 01:05:15 +0800

    Added file util

--- emacs/emacs.d/xtang-editing.el
+++ emacs/emacs.d/xtang-editing.el
@@ -26,6 +26,14 @@
   (add-hook hook
             (lambda () (setq show-trailing-whitespace nil))))
 
+(add-hook 'before-save-hook
+          (lambda ()
+            (when buffer-file-name
+              (let ((dir (file-name-directory buffer-file-name)))
+                (when (and (not (file-exists-p dir))
+                           (y-or-n-p (format "Directory %s does not exists. Create it?" dir)))
+                  (make-directory dir t))))))
+
 (defun esk-local-column-number-mode ()
   (make-local-variable 'column-number-mode)
   (column-number-mode t))
