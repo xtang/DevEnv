effde0c70f974305b3eedcf6efb0d4f2be6420f1
xtang <xiaom.tang@gmail.com>
2014-03-24 22:53:52 +0800

    Use solarized theme

--- emacs/emacs.d/xtang-theme.el
+++ emacs/emacs.d/xtang-theme.el
@@ -1,25 +1,25 @@
 (require-package 'zenburn-theme)
 
 ;; patch
-(load-theme 'zenburn t)
-(zenburn-with-color-variables
-  (custom-theme-set-faces
-   'zenburn
-   `(highlight ((t (:background ,zenburn-bg+3))))
-   `(region ((,class (:background ,zenburn-bg+3))
-             (t :inverse-video t)))
-   `(hl-line-face ((,class (:background ,"gray18"))
-                   (t :weight bold)))
-   `(hl-line ((,class (:background ,"gray18"))
-              (t :weight bold)))))
+(load-theme 'solarized-dark t)
+;; (zenburn-with-color-variables
+;;   (custom-theme-set-faces
+;;    'zenburn
+;;    `(highlight ((t (:background ,zenburn-bg+3))))
+;;    `(region ((,class (:background ,zenburn-bg+3))
+;;              (t :inverse-video t)))
+;;    `(hl-line-face ((,class (:background ,"gray18"))
+;;                    (t :weight bold)))
+;;    `(hl-line ((,class (:background ,"gray18"))
+;;               (t :weight bold)))))
 
 (fringe-mode (cons 1 1))
 
 ;; change the font and height
 (when (and *is-a-mac*
            window-system)
-  (set-face-attribute 'default nil :font "Monaco" :height
-                      (if *is-retina* 142 125)))
+  (set-face-attribute 'default nil :font "Source Code Pro" :height
+                      (if *is-retina* 152 135)))
 
 (set-fontset-font t 'han (font-spec :name "STHeiti"))
 
