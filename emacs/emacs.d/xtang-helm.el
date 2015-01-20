df70677a994989fe5942b9ba035c90be797e9567
xtang <xiaom.tang@gmail.com>
2015-01-20 15:58:44 +0800

    Added show king-ring *a*, **b**

--- emacs/emacs.d/xtang-helm.el
+++ emacs/emacs.d/xtang-helm.el
@@ -1,6 +1,7 @@
 (require-package 'helm)
 
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "M-RET") 'helm-for-files)
 (setq helm-split-window-default-side "right")
